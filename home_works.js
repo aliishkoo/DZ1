fa118c66a5b283d399dab1a6dcf7c5f3fe0edab2
aliishkoo <aliakasymova148@gmail.com>
2024-12-05 12:22:02 +0600

    Add files via upload

---
+++ home_works.js
@@ -0,0 +1,35 @@
+// GMAIL BLOCK
+
+const gmailInput = document.querySelector('#gmail_input');
+const gmailButton = document.querySelector('#gmail_button');
+const gmailResult = document.querySelector('#gmail_result');
+
+const regExp = /^[a-zA-Z0-9._]+@gmail\.com$/
+
+gmailButton.onclick = () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = "OK"
+        gmailResult.style.color = "green"
+    } else {
+        gmailResult.innerHTML = "Invalid gmail format"
+        gmailResult.style.color = "red"
+    }
+}
+
+const childBlock = document.querySelector('.child_block');
+const parentBlock = document.querySelector('.parent_block');
+
+let position = 0;
+
+const moveBlock = () => {
+    const parentWidth = parentBlock.offsetWidth;
+    const childWidht = childBlock.offsetWidth;
+
+    if (position < parentWidth - childWidht) {
+        position++
+        childBlock.style.left = `${position}px`
+        requestAnimationFrame(moveBlock);
+    }
+};
+
+moveBlock();
